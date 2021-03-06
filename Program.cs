bbf9d9b7e508154d0c694689bbd486fa65ef841f
jatindhingra <80097813+jatindhingra@users.noreply.github.com>
2021-03-06 10:50:20 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FibonacciCheck
+{
+    class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            try
+            {
+                int n1 = 0, n2 = 1, n3, i, n;
+                n = 8;
+                Console.WriteLine("Fibonacci series:");
+                Console.Write(n1 + " " + n2 + " ");
+                for (i = 2; i < n; ++i)
+                {
+                    n3 = n1 + n2;
+                    Console.Write(n3 + " ");
+                    n1 = n2;
+                    n2 = n3;
+                }
+
+                Console.WriteLine(" \n Test Case");
+                Console.WriteLine("Enter a number you want to test :");
+                int x = Convert.ToInt32(Console.ReadLine());
+                TestCases tc = new TestCases();
+                Console.WriteLine(tc.isFibonacci(x) ? x + " is a Fibonacci Number" : x + " is a not Fibonacci Number");
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+    }
+}
---
+++ TestCases.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FibonacciCheck
+{
+    class TestCases
+    {
+        public bool isPerfectSquare(int x)
+        {
+            int s = (int)Math.Sqrt(x);
+            return (s * s == x);
+        }
+
+       
+        public bool isFibonacci(int n)
+        {
+            return isPerfectSquare(5 * n * n + 4) ||
+                   isPerfectSquare(5 * n * n - 4);
+        }
+      
+
+    }
+}
